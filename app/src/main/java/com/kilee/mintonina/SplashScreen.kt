00f4d8cb2a896b2ef2ee2d176dc3374db3dd7af7
rifkialdi <rifkialdiansyah43@gmail.com>
2022-06-12 08:20:03 +0700

    update splash screen

--- app/src/main/java/com/kilee/mintonina/SplashScreen.kt
+++ app/src/main/java/com/kilee/mintonina/SplashScreen.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,9 +13,10 @@ class SplashScreen : AppCompatActivity() {
 
         supportActionBar?.hide()
 
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
               val intent = Intent(this@SplashScreen, MainActivity::class.java)
             startActivity(intent)
+            finish()
         },2000)
     }
 }
